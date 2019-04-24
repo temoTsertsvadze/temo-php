92974c5a8872b542b25a247dda3d547000284c28
temuka <temo.tsertsvadze.gtu@gmail.com>
2019-04-25 00:50:03 +0400

    temo

---
+++ Lab1/Davaleba 1/index.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+<form action="form.php" method ="GET">
+
+<label>name:</label><br> 
+<input type="text" name="name"><br>
+<label>lastname: </label><br>
+<input type="text" name="lastname"><br>
+<label>salary: </label><br>
+<input type="text" name="salary"><br>
+<label>position: </label><br> 
+<input type="text" name="position" ><br>
+<input type="submit"/><br>
+</form>
+</div>
+
+
+
+</body>
+</html>
